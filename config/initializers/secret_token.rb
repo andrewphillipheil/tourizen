b3e07aac48adacb934776f4985a14695586c7053
testing <andrew@jsp710569.ariens.corporate>
2015-03-13 12:34:57 -0400

    new branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tourizen::Application.config.secret_key_base = '8d5beaca96fe9377bbb207be7a75227f374a49f616a4a055725b2a1e96caa1d2f785e0c50c2332b917f1b058bd4c1573223da7c613506e27871d6662d60fdb6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tourizen::Application.config.session_store :cookie_store, key: '_tourizen_session'
